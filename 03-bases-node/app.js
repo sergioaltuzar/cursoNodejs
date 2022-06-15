5909b61ff45ee1eec7d5c970efd53051c058d095
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-06-15 16:22:36 -0500

    agregando

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -2,8 +2,13 @@
 const {crearArchivo} = require('./helpers/multiplicar');
 
 console.clear();
+const [ , , arg3 = 'base=5'] = process.argv;
+const [ , base = 5] = arg3.split('=');
 
-const base =5;
+
+
+
+// const base =5;
 
 crearArchivo(base)
     .then(nombreArchivo => console.log(`Archivo creado: ${nombreArchivo}`))
