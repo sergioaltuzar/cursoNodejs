cbbb660f5be2d5ec56d9fdf0aca729a44d63f2c3
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-06-16 12:30:01 -0500

    sumary

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,15 +1,13 @@
-
 const {crearArchivo} = require('./helpers/multiplicar');
-
+const argv = require('yargs').argv;
 console.clear();
-const [ , , arg3 = 'base=5'] = process.argv;
-const [ , base = 5] = arg3.split('=');
+console.log(process.argv);
+console.log(argv);
+console.log('base: yarg', argv.base);
 
-
-
-
+// const [ , , arg3 = 'base=5'] = process.argv;
+// const [ , base = 5] = arg3.split('=');
 // const base =5;
-
-crearArchivo(base)
-    .then(nombreArchivo => console.log(`Archivo creado: ${nombreArchivo}`))
-    .catch(err => console.log(err));
+// crearArchivo(base)
+//     .then(nombreArchivo => console.log(`Archivo creado: ${nombreArchivo}`))
+//     .catch(err => console.log(err));
